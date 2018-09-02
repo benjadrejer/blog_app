e579c2adc0734bda80284fc003de1f6f26b4d4b3
Benjamin Drejer <benjadrejer@gmail.com>
2018-09-02 22:24:06 +0200

    Finished blog app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var methodOverride = require('method-override');
+var expressSanitizer = require('express-sanitizer');
 var app = express();
 
 mongoose.connect('mongodb://localhost/restful_blog_app', { useNewUrlParser: true });
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost/restful_blog_app', { useNewUrlParser: true
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
@@ -40,6 +42,7 @@ app.get('/blogs/new', function(req, res) {
 });
 
 app.post('/blogs', function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, function(err, newBlog) {
     if (err) {
       res.render('new');
@@ -70,6 +73,7 @@ app.get('/blogs/:id/edit', function(req, res) {
 });
 
 app.put('/blogs/:id', function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, post) {
     if (err) {
       res.redirect('/blogs');
